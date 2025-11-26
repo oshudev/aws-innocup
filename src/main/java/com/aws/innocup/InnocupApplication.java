3b4eecb7a86fc0ac73142aaaea61afa336da2ce2
oshu <99813212+oshudev@users.noreply.github.com>
2025-11-26 14:45:04 +0800

    Initial commit

---
+++ src/main/java/com/aws/innocup/InnocupApplication.java
@@ -0,0 +1,13 @@
+package com.aws.innocup;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InnocupApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InnocupApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/aws/innocup/InnocupApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aws.innocup;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InnocupApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
